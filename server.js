636e5eb5d9fb36e6f3f7a7e4ab68de5709d43347
Jezer Chilel <isafrus@gmail.com>
2017-02-08 09:21:09 +0000

    moment library added

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.listen(8080, function() {
+const express = require('express');
+const app = express();
+const moment = require('moment');
+
+app.get('/:date', (req, res) => {
+  let date = moment( req.params.date, 'MMMM DD, YYYY', true);
+  
+  if (!date.isValid()) {
+    date = moment.unix(req.params.date);
+  }
+  
+  if (!date.isValid()) {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+  
+  res.json({
+    unix: date.format('X'),
+    natural: date.format('MMMM DD, YYYY')
+  });
+});
+
+app.listen(8080, () => {
   console.log('Server running on port 8080');
 });
